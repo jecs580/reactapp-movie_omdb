c3a7ec04f1cb8247145f6e034a83914e6180f59a
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-04-30 20:45:50 -0400

    Usando la API de omdb para obtener las peliculas y agregando un buscador de peliculas

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -7,7 +7,7 @@ class Card extends React.Component {
         return (
             <div className="col-md-4">
                 <div className="card">
-                    <img src={movie.Poster} className="card-img-top" width="100" />
+                    <img src={movie.Poster !== 'N/A' ? movie.Poster:'https://laspalmas.com.bo/wp-content/uploads/2016/06/Sin_imagen_disponible.jpg'} alt={movie.Title} className="card-img-top" width="100" />
                     <div className="card-body">
                         <h4>{movie.Year} {movie.Title}</h4>
                         <p>{movie.Type}</p>
--- src/containers/list.js
+++ src/containers/list.js
@@ -1,33 +1,73 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Card from "../components/card/card";
 
+const API = 'http://www.omdbapi.com/?i=tt3896198&apikey=a0a6b836';
 class List extends React.Component {
 
-    constructor(){
-        super();  // Primeramente obtenemos los metodos que nos da react
-        this.state={
-            data:[]
-        }
+  constructor() {
+    super();  // Primeramente obtenemos los metodos que nos da react
+    this.state = {
+      data: [],
+      searchTerm:'',
+      error:'',
     }
-    //Se ejecutara en cuanto el componente es mostrado en pantalla
-    async componentDidMount() {
-        const res = await fetch('../../assets/data.json');
-        const resJSON = await res.json();
-        this.setState({
-            data:resJSON
-        })
+  }
+  //Se ejecutara en cuanto el componente es mostrado en pantalla
+  async componentDidMount() {
+    // const res = await fetch('../../assets/data.json');
+    const res = await fetch(`${API}&s=batman`)
+    const resJSON = await res.json();
+    this.setState({
+      data: resJSON.Search
+    })
+
+  }
+
+  async handleSubmit(e){
+    e.preventDefault(); // Cancelamos el evento de enviar al momento de dar Enter.
+    if(!this.state.searchTerm){
+      // Condicion cuando searchTerm este vacio
+      return this.setState({error:'Por favor escribe un texto valido'})
     }
 
-    render(){
-        return <div className="row">
-            {
-                // Las llaves indican que dentro se tiene codigo js.
-                this.state.data.map(movie =>{
-                    return <Card movie={movie} key={movie.id}/>
-                })
-            }
-        </div>
+    const res = await fetch(`${API}&s=${this.state.searchTerm}`);
+    const data = await res.json(); 
+    if(!data.Search){
+      return this.setState({error:'No hay resultados'});
+      
     }
+    this.setState({
+      data:data.Search,
+      error:'',
+      searchTerm:''
+    })
+    
+  }
+  render() {
+    return (
+      <Fragment>
+        <div className="row">
+          <div className="col-md-4 offset-md-4 p-4">
+            {/* Usamos el offset-md-4 para centrar el contenido */}
+            <form onSubmit={(e) => this.handleSubmit(e)} action="">
+              {/* Si solo tenemos un elemento dentro del form(input) al precionar Enter, el form se enviara por defecto */}
+              <input type="text" className="form-control" placeholder="search" onChange={e=>this.setState({searchTerm:e.target.value})} autoFocus value={this.state.searchTerm}/>
+            </form>
+            <p className="text-white">{this.state.error ? this.state.error :''}</p>
+            {/* Usamos un operador termario para hacer la condicional en una linea */}
+          </div>
+        </div>
+        <div className="row">
+          {
+            // Las llaves indican que dentro se tiene codigo js.
+            this.state.data.map(movie => {
+              return <Card movie={movie} key={movie.imdbID} />
+            })
+          }
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 export default List
